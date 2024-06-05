e638cd808c1792753d0fee9dde4fcb586e2ae09c
Rushin Shah <shahrushin32@gmail.com>
2024-06-05 15:54:16 +0530

    debugging on production.

--- backend/app.js
+++ backend/app.js
@@ -22,7 +22,10 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 
-
+app.use((req, res, next) => {
+    console.log(res.cookies)
+    next();
+})
 
 // Defining different Routes
 app.use("/notes", NoteRoute);
