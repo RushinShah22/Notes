ed4fe66695be75e4b55730922f5b18aeea2a11a6
Rushin Shah <shahrushin32@gmail.com>
2024-06-05 14:46:29 +0530

    updated cors setting.

--- backend/app.js
+++ backend/app.js
@@ -14,10 +14,7 @@ dotenv.config({
 });
 const app = express();
 
-app.use(cors({
-    credentials: true,
-    origin: true,
-}));
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
