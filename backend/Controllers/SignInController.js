5325cae6fc6307f39dbae9131b312c9a8f91f666
Rushin Shah <shahrushin32@gmail.com>
2024-06-05 15:58:06 +0530

    debugging on production.

--- backend/Controllers/SignInController.js
+++ backend/Controllers/SignInController.js
@@ -6,12 +6,12 @@ module.exports.SignInUser = async (req, res) => {
     const token = AuthController.createJwt(req.body.id);
     res.cookie("jwt", token,  {
         httpOnly: true,
+        secure: true,
         maxAge: 3 * 60 * 60 * 24 * 1000
     })
     
     const userDetails = await UserModel.findById(req.body.id).select("firstName lastName _id");
-    res.cookie("firstName", userDetails.firstName)
-    res.cookie("lastName", userDetails.lastName);
+    
     res.status(201).json({
         status: "Success",
         userDetails
